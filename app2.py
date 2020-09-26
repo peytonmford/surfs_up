5398ff1b5e7ca43a89a0aa32bf8d54e77474cb0d
peytonmford <peyton.m.ford@ttu.edu>
2020-09-26 12:38:03 -0500

    Added June and December data

---
+++ app2.py
@@ -0,0 +1,38 @@
+# Import dependencies
+import datetime as dt
+import numpy as np
+import pandas as pd
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask, jsonify
+
+
+# Create engine for database
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+
+# Save references for each table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# Create session link
+session = Session(engine)
+
+app = Flask(__name__)
+
+@app.route("/")
+
+def welcome():
+    return(
+    '''
+    Welcome to the Climate Analysis API!
+    Available Routes:
+    /api/v1.0/precipitation
+    /api/v1.0/stations
+    /api/v1.0/tobs
+    /api/v1.0/temp/start/end
+    ''')
